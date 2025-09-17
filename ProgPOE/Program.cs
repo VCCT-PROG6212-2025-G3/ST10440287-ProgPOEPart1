7ba0c4fb0f0c7cd0c9713a579698658d836ef4d2
Brett James-ST10440287 <brettjames43@gmail.com>
2025-09-17 21:44:56 +0200

    Views are all completed, comments are added and refrences of information I used to guide me through out creating the GUI.

--- ProgPOE/Program.cs
+++ ProgPOE/Program.cs
@@ -53,3 +53,9 @@ namespace ProgPOE
         }
     }
 }
+//Refernces
+// - ASP.NET Core Documentation: https://docs.microsoft.com/en-us/aspnet/core/?view=aspnetcore-6.0
+// - Entity Framework Core Documentation: https://docs.microsoft.com/en-us/ef/core/
+// - Bootstrap Documentation: https://getbootstrap.com/docs/5.1/getting-started/introduction/
+// - jQuery Documentation: https://api.jquery.com/
+// - ChatGPT https://chatgpt.com/share/68cb0efe-b6d4-8001-8e7b-dd56bf04cc8a
